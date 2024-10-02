17a838c91a165cdaab28d48317f1c04c1510dd03
Andre Francez <andregfrancez@gmail.com>
2024-10-01 22:39:56 -0300

    Create AuthController for user authentication

---
+++ src/Controllers/AuthController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using VehiclesAPI.Dto;
+using VehiclesAPI.Interfaces;
+using VehiclesAPI.Models;
+
+namespace VehiclesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(ITokenService tokenService, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration) : ControllerBase
+    {
+        private readonly ITokenService _tokenService = tokenService;
+        private readonly UserManager<ApplicationUser> _userManager = userManager;
+        private readonly RoleManager<IdentityRole> _roleManager = roleManager;
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login(LoginDTO loginDTO)
+        {
+            var user = await _userManager.FindByNameAsync(loginDTO.UserName);
+
+            if(user is not null && await _userManager.CheckPasswordAsync(user, loginDTO.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName!),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+                foreach(var role in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, role));
+                }
+
+                var token = _tokenService.GetJwtAccessToken(authClaims, _configuration);
+
+                var refreshToken = _tokenService.GenerateRefreshToken();
+
+                _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInMinutes"], out int refreshTokenValidityInMinutes);
+
+                user.RefreshToken = refreshToken;
+
+                user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMinutes(refreshTokenValidityInMinutes);
+
+                await _userManager.UpdateAsync(user);
+
+                return Ok(new
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    RefreshToken = refreshToken,
+                    Expiration = token.ValidTo
+                });
+            }
+
+            return Unauthorized();
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            var userExists = await _userManager.FindByNameAsync(registerDTO.UserName);
+
+            if (userExists != null)
+                return Conflict("User already exists");
+
+            ApplicationUser user = new()
+            {
+                Email = registerDTO.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerDTO.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if(!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new ResponseDTO { Status = "Error", Message = "User creation failed" });
+
+            return Created();
+        }
+
+        [HttpPost]
+        [Route("Refresh-token")]
+        public async Task<IActionResult> RefreshToken(TokenDTO tokenDTO)
+        {
+            if (tokenDTO is null)
+                return BadRequest("Invalid client request");
+
+            string? accessToken = tokenDTO.AccessToken ?? throw new ArgumentNullException(nameof(tokenDTO));
+
+            string? refreshToken = tokenDTO.RefreshToken ?? throw new ArgumentException(nameof(tokenDTO));
+
+            var principal = _tokenService.GetPrincipalFromExpiredToken(accessToken!, _configuration);
+
+            if (principal is null)
+                return BadRequest("Invalid access");
+
+            string username = principal.Identity.Name;
+
+            var user = await _userManager.FindByNameAsync(username!);
+
+            if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.UtcNow)
+                return BadRequest("Invalid access");
+
+            var newAccessToken = _tokenService.GetJwtAccessToken(principal.Claims.ToList(), _configuration);
+
+            var newRefreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+            await _userManager.UpdateAsync(user);
+
+            return new ObjectResult(new
+            {
+                accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                refreshToken = newRefreshToken
+            });
+        }
+
+        [Authorize]
+        [HttpPost]
+        [Route("Revoke/{username}")]
+        public async Task<IActionResult> Revoke(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user is null)
+                return BadRequest("Invalid username");
+
+            user.RefreshToken = null;
+            await _userManager.UpdateAsync(user);
+
+            return NoContent();
+        }
+    }
+}
--- src/Models/ApplicationUser.cs
+++ src/Models/ApplicationUser.cs
@@ -4,6 +4,6 @@ namespace VehiclesAPI.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    public string RefreshToken { get; set; }
+    public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpiryTime { get; set; }
 }
